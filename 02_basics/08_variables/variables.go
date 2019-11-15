6676997d47d51271ead6397811195efb3bd9a838
poppen <poppen.jp@gmail.com>
2019-11-15 10:52:05 +0900

    feat: initial commit

---
+++ 02_basics/08_variables/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+	fmt.Println("test")
+	fmt.Println("test")
+	fmt.Println("test")
+}
+
+func a() {
+}
---
+++ 02_basics/11_basic_types/basic-types.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	toBe   bool       = false
+	maxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func main() {
+	fmt.Printf("Type: %T Value: %v\n", toBe, toBe)
+}
