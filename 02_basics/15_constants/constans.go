8c6e9bbf32279cb329c93f8a694bff637ca7107b
poppen <poppen.jp@gmail.com>
2019-11-18 08:54:01 +0900

    feat: add files haven't commited yet

---
+++ 02_basics/15_constants/constans.go
@@ -0,0 +1,19 @@
+// vim: fenc=utf8:
+// 鶴亀
+
+package main
+
+import (
+	"fmt"
+)
+
+const pi = 3.14
+
+func main() {
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
