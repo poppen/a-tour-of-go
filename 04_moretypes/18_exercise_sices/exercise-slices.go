90b170bfdf0ddff8ea1f9e69dbd0b8880f82b1e8
poppen <poppen.jp@gmail.com>
2019-11-18 22:40:17 +0900

    feat: update "Exercise Slices"

--- 04_moretypes/18_exercise_sices/exercise-slices.go
+++ 04_moretypes/18_exercise_sices/exercise-slices.go
@@ -7,10 +7,17 @@ import (
 func genPic(dx, dy int) [][]uint8 {
 	p := make([][]uint8, dy)
 
+	// for i := 0; i < dy; i++ {
+	// 	p[i] = make([]uint8, dx)
+	// 	for j := 0; j < dx; j++ {
+	// 		p[i][j] = uint8(i * j)
+	// 	}
+	// }
+
 	for i := range p {
 		p[i] = make([]uint8, dx)
 		for j := range p[i] {
-			p[i][j] = uint8(dx * dy)
+			p[i][j] = uint8(i * j)
 		}
 	}
 
