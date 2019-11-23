0d8ad6faa02e99c3535e73d03671886b65bb5a4c
poppen <poppen.jp@gmail.com>
2019-11-23 10:03:36 +0900

    refactor: improve "Exercise: Equivalent Binary Trees"

--- 06_concurrency/07_exercise_equivalent_binary_trees/exercise-equivalent-binary-trees.go
+++ 06_concurrency/07_exercise_equivalent_binary_trees/exercise-equivalent-binary-trees.go
@@ -10,10 +10,10 @@ import (
 func walk(t *tree.Tree, ch chan int) {
 	ch <- t.Value
 	if t.Left != nil {
-		go walk(t.Left, ch)
+		walk(t.Left, ch)
 	}
 	if t.Right != nil {
-		go walk(t.Right, ch)
+		walk(t.Right, ch)
 	}
 }
 
@@ -24,12 +24,20 @@ func same(t1, t2 *tree.Tree) bool {
 	v1 := make([]int, 10)
 	v2 := make([]int, 10)
 
-	go walk(t1, c1)
-	go walk(t2, c2)
+	go func() {
+		walk(t1, c1)
+		close(c1)
+	}()
+	go func() {
+		walk(t2, c2)
+		close(c2)
+	}()
 
-	for i := 0; i < 10; i++ {
-		v1[i] = <-c1
-		v2[i] = <-c2
+	for v := range c1 {
+		v1 = append(v1, v)
+	}
+	for v := range c2 {
+		v2 = append(v2, v)
 	}
 
 	if len(v1) != len(v2) {
@@ -39,7 +47,7 @@ func same(t1, t2 *tree.Tree) bool {
 	sort.Sort(sort.IntSlice(v1))
 	sort.Sort(sort.IntSlice(v2))
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < len(v1); i++ {
 		if v1[i] != v2[i] {
 			return false
 		}
@@ -55,5 +63,6 @@ func main() {
 		fmt.Println(<-ch)
 	}
 
-	fmt.Printf("%v\n", same(tree.New(1), tree.New(1)))
+	fmt.Printf("tree.New(1) and tree.New(1) are same?: %v\n", same(tree.New(1), tree.New(1)))
+	fmt.Printf("tree.New(1) and tree.New(2) are same?: %v\n", same(tree.New(1), tree.New(2)))
 }
