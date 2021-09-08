b7fba65d51b91df60bde523b10bef7248ffde325
Shinsuke MATSUI <smatsui@valeria.local>
2021-09-08 21:28:07 +0900

    update: small update

--- 01_welcome/01_hello/hello.go
+++ 01_welcome/01_hello/hello.go
@@ -3,6 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
-	a
+	fmt.Println("Hello, 世界 with vim")
 }
--- 01_welcome/02_sandbox/sandbox.go
+++ 01_welcome/02_sandbox/sandbox.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to the playground")
-	fmt.Println("The time is", time.Now())
+	fmt.Println("Welcome to the sandbox!")
+	fmt.Println("The Time is", time.Now())
 }
